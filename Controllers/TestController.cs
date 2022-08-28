a7ed638f4f46a8e68be0777c0269cb1c8d2bbd0b
Arvin Jayson Tamayo Castro <email@example.com>
2022-08-28 12:24:43 +0800

    This is now working

---
+++ Controllers/TestController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Data;
+using TodoApp.Models;
+
+// [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    private readonly ApiDbContext _context;
+
+    public TestController(ApiDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult GetItems()
+    {
+        var message = "Perfect";
+        return Ok(message);
+    }
+}
